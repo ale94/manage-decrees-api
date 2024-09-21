babac493d1d3447be8a38b32ebb7d424f8a63ab1
ale94 <alejandrorua111@gmail.com>
2024-09-20 23:30:36 -0300

    init: first commit

---
+++ src/main/java/ar/com/ale94/manage_decrees/ManageDecreesApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ale94.manage_decrees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDecreesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageDecreesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ale94/manage_decrees/ManageDecreesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ale94.manage_decrees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageDecreesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
