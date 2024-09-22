2779fd611623016b75c10ec9dad165b28430fa9b
ale94 <alejandrorua111@gmail.com>
2024-09-21 23:26:04 -0300

    feat: get method controller created

--- src/main/java/ar/com/ale94/manage_decrees/controllers/DecreeController.java
+++ src/main/java/ar/com/ale94/manage_decrees/controllers/DecreeController.java
@@ -1,4 +1,22 @@
 package ar.com.ale94.manage_decrees.controllers;
 
+import ar.com.ale94.manage_decrees.entities.DecreeEntity;
+import ar.com.ale94.manage_decrees.services.IDecreeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class DecreeController {
+
+    private final IDecreeService service;
+
+    @GetMapping
+    public ResponseEntity<List<DecreeEntity>> get() {
+        return ResponseEntity.ok(service.getAll());
+    }
 }
